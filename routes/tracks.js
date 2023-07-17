6734e71f8c9fd9c1e7b8b1f94944afa8c6a501fe
LuisUcla <caluigi7@gmail.com>
2023-07-17 12:31:19 -0400

    feat: add tracks routes with validators

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,9 +1,16 @@
 const express = require('express')
-const { getItems, createItem } = require('../controllers/tracks')
+const { validatorCreateItem, validatorGetItem } = require('../validators/tracks');
+const { getItems, createItem, getItem, updateItem, deleteItem } = require('../controllers/tracks')
 const router = express.Router()
 
-router.get('/', getItems);
+router.get('/', getItems); // lista de items
 
-router.post('/', createItem)
+router.post('/', validatorCreateItem, createItem) // crear item
+
+router.get('/:id', validatorGetItem, getItem)
+
+router.put('/:id', validatorGetItem, validatorCreateItem, updateItem) // se usan dos middleware para validar el id y la data
+
+router.delete('/:id', validatorGetItem, deleteItem)
 
 module.exports = router
