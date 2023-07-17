8859590b03d6c6bba30bcf339bb97360e8d043ea
LuisUcla <caluigi7@gmail.com>
2023-07-17 12:34:26 -0400

    feat: add logic of track controller and methods
    se agrega el metodo matchedData para limpiar la data en caso de venir erronea desde el fronted

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -1,26 +1,60 @@
-const { tracksModel } = require('../models')
+const { matchedData } = require('express-validator');
+const { tracksModel } = require('../models');
+const { handleHttpError } = require('../utils/handleError');
 
 const getItems = async (req, res) => {
-    const data = await tracksModel.find({});
-    res.send({ data })
+    try {     
+        const data = await tracksModel.find({});
+        res.send({ data })
+    } catch (e) {
+        handleHttpError(res, "Error_get_items")
+    }
 }
 
-const getItem = (req, res) => {
+const getItem = async (req, res) => {
+    try {
+        req = matchedData(req);
+        const { id } = req;
+        const data = await tracksModel.findById(id);
+        res.send({ data })
+    } catch (e) {
+        handleHttpError(res, "ERROR_GET_ITEM")
+    }
 
 }
 
 const createItem = async (req, res) => {
-    const {body} = req
-    const data = await tracksModel.create(body)
-    res.send(data)
+    try {
+        const body = matchedData(req) // matchedData: limpia los datos en caso de que vengan datos demas
+        const data = await tracksModel.create(body)
+        res.send({ data });
+    } catch (e) {
+        handleHttpError(res, "Error_create_item")
+    }
 }
 
-const updateItem = (req, res) => {
-
+const updateItem = async (req, res) => {
+    try {
+        const {id, ...body} = matchedData(req) // matchedData: limpia los datos en caso de que vengan datos demas
+        const data = await tracksModel.findByIdAndUpdate(id, body);
+        res.send({ data });
+    } catch (e) {
+        handleHttpError(res, "Error_update_item_tracks")
+    }
 }
 
-const deleteItem = (req, res) => {
-
+const deleteItem = async (req, res) => {
+    try {
+        req = matchedData(req);
+        const { id } = req;
+        const data = await tracksModel.deleteOne({_id: id}); // deleteOne elimina de forma fisica
+        /* 
+            cuando se use eliminacion logica debe ser con 'tracksModel.delete'
+        */
+        res.send({ data })
+    } catch (e) {
+        handleHttpError(res, "ERROR_DELETE_ITEM")
+    }
 }
 
 module.exports = { getItems, getItem, createItem, updateItem, deleteItem }
