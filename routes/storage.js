a535861f361ca6ceae708f1eb3ea608866a91f71
LuisUcla <caluigi7@gmail.com>
2023-07-17 12:30:27 -0400

    feat: add storage routes

--- routes/storage.js
+++ routes/storage.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router();
 const uploadMiddleware = require('../utils/handleStorage');
-const { createItem } = require('../controllers/storage');
+const { validatorGetItem } = require('../validators/storages')
+const { createItem, getItem, getItems, deleteItem } = require('../controllers/storage');
 
+router.get('/', getItems)
+
+router.get('/:id', validatorGetItem, getItem);
+
+router.put('/:id', validatorGetItem, getItem);
+
+router.delete('/:id', validatorGetItem, deleteItem);
 
 router.post('/', uploadMiddleware.single('myfile'), createItem)
 
