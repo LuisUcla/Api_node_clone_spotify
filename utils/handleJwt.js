6f8f5ba17d4223fe94436b9213fd41b8a0293bcd
LuisUcla <caluigi7@gmail.com>
2023-07-20 09:59:00 -0400

    feat: add handleJwt method
    Metodos para para firmar el token y generarlo y para verificar cuando se hace una peticion http

---
+++ utils/handleJwt.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET
+
+const tokenSign = async (user) => {
+    const sign = jwt.sign(
+        {
+            _id: user._id,
+            role: user.role
+        },
+        JWT_SECRET,
+        {
+            expiresIn: '2h'
+        }
+    );
+
+    return sign;
+
+} 
+
+const verifyToken = (tokenJwt) => { // verifica que el token este firmado por el usuario o jwt
+    try {
+        return jwt.verify(tokenJwt, JWT_SECRET);
+    } catch (e) {
+        return null
+    }
+}
+
+module.exports = { tokenSign, verifyToken }
