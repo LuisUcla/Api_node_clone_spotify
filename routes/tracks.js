46bd41181f39b7f5c6bf2d624d9740386682a92d
LuisUcla <caluigi7@gmail.com>
2023-07-21 08:07:14 -0400

    feat: add middleware of auth in the routes tracks

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const { validatorCreateItem, validatorGetItem } = require('../validators/tracks');
-const { getItems, createItem, getItem, updateItem, deleteItem } = require('../controllers/tracks')
+const { getItems, createItem, getItem, updateItem, deleteItem } = require('../controllers/tracks');
+const authMiddleware = require('../middleware/session');
 const router = express.Router()
 
-router.get('/', getItems); // lista de items
+router.get('/', authMiddleware, getItems); // lista de items con middleware de inicio de sesion
 
 router.post('/', validatorCreateItem, createItem) // crear item
 
