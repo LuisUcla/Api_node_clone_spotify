7712abeb02462b70bf5ef4bee6438aeee61447f5
LuisUcla <caluigi7@gmail.com>
2023-07-26 08:09:36 -0400

    refactor: change routes y add middleware auth and checkRol

--- routes/tracks.js
+++ routes/tracks.js
@@ -2,16 +2,17 @@ const express = require('express')
 const { validatorCreateItem, validatorGetItem } = require('../validators/tracks');
 const { getItems, createItem, getItem, updateItem, deleteItem } = require('../controllers/tracks');
 const authMiddleware = require('../middleware/session');
+const checkRol = require('../middleware/rol');
 const router = express.Router()
 
 router.get('/', authMiddleware, getItems); // lista de items con middleware de inicio de sesion
 
-router.post('/', validatorCreateItem, createItem) // crear item
+router.post('/', authMiddleware, checkRol(['admin']), validatorCreateItem, createItem) // crear item
 
-router.get('/:id', validatorGetItem, getItem)
+router.get('/:id',authMiddleware, validatorGetItem, getItem)
 
-router.put('/:id', validatorGetItem, validatorCreateItem, updateItem) // se usan dos middleware para validar el id y la data
+router.put('/:id',authMiddleware, validatorGetItem, validatorCreateItem, updateItem) // se usan dos middleware para validar el id y la data
 
-router.delete('/:id', validatorGetItem, deleteItem)
+router.delete('/:id',authMiddleware, validatorGetItem, deleteItem)
 
 module.exports = router
