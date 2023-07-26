ee1c4076d68e079bf56eb01eb2f276c4643547c5
LuisUcla <caluigi7@gmail.com>
2023-07-26 08:11:16 -0400

    refactor: add use morganBody library
    MorganBody es para imprimir por consola los errores de 400

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 require("dotenv").config(); // Para usar las variables de entorno
 const express = require("express");
 const cors = require('cors');
+// const morganBody = require('morgan-body');
 const dbConnect = require('./config/mongo')
 const app = express();
 
 app.use(cors());
 app.use(express.json()); // cuando se hace el controlador
 app.use(express.static('storage')); // para ver los archivos estaticos en el navegador
+// morganBody(app, {
+
+// })
 
 const port = process.env.PORT || 3000; // usa la variable de entorno
 
